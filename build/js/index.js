243499620fb838bbad25c228974aecfa7017a711
EsTeBaN22001 <esteban1.redon2@gmail.com>
2021-08-22 21:15:09 -0300

    Se agregó el JQuery para la nav por tabs del programa-evento

--- build/js/index.js
+++ build/js/index.js
@@ -10,3 +10,28 @@ L.marker([-33.273946, -426.324935]).addTo(map)
   .openPopup()
   .bindTooltip('Un tooltip')
   .openTooltip();
+
+
+//-----------------------------------------------------------------------//
+//------------------------------- Jquery -------------------------------//
+//---------------------------------------------------------------------//
+
+
+// Código para los tabs del programa del evento en la páina index.html
+$(document).ready(function(){
+
+  $('div.hidden').hide();
+
+  $('.program-event .course-info:first').show();
+  $('.program-menu a:first').addClass('active');
+
+  $('.program-menu a').on('click', function(){
+    $('.program-menu a').removeClass('active');
+    $(this).addClass('active');
+    $('.hidden').hide();
+    const enlace = $(this).attr('href');
+    $(enlace).fadeIn(1000);
+
+    return false;
+  })
+})
