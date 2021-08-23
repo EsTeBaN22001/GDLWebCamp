ad4240cdde419369147cc6490afd56975ac65e34
EsTeBaN22001 <esteban1.redon2@gmail.com>
2021-08-22 22:40:03 -0300

    Se agregó la libreria para añadir la cuenta regresiva a los números de la página del index

--- build/js/index.js
+++ build/js/index.js
@@ -17,9 +17,8 @@ L.marker([-33.273946, -426.324935]).addTo(map)
 //---------------------------------------------------------------------//
 
 
-// Código para los tabs del programa del evento en la páina index.html
 $(document).ready(function(){
-
+  // Código para los tabs del programa del evento en la páina index.html
   $('div.hidden').hide();
 
   $('.program-event .course-info:first').show();
@@ -33,5 +32,20 @@ $(document).ready(function(){
     $(enlace).fadeIn(1000);
 
     return false;
+  })
+
+
+  // Animación para los números
+  $('.event-summary li:nth-child(1) p').animateNumber({number: 6}, 3000);
+  $('.event-summary li:nth-child(2) p').animateNumber({number: 15}, 3000);
+  $('.event-summary li:nth-child(3) p').animateNumber({number: 3}, 3000);
+  $('.event-summary li:nth-child(4) p').animateNumber({number: 9}, 3000);
+
+  // Cuenta regresiva
+  $('.count-down').countdown('2021/12/10 09:00:00', function(event){
+    $('#days').html(event.strftime('%D'));
+    $('#hours').html(event.strftime('%H'));
+    $('#minutes').html(event.strftime('%M'));
+    $('#seconds').html(event.strftime('%S'));
   })
 })
