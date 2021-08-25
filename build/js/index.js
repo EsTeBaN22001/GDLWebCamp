093393dfdcb210225fcf0484ee6b5e32425f1048
EsTeBaN22001 <esteban1.redon2@gmail.com>
2021-08-25 00:13:56 -0300

    Se agregó la funcionalidad para el menu de hamburguesa para móviles

--- build/js/index.js
+++ build/js/index.js
@@ -1,16 +1,16 @@
 if(document.getElementById('map')){
   // Mapa de leaflet.js
-var map = L.map('map').setView([-33.273946, -426.324935], 17);
+  var map = L.map('map').setView([-33.273946, -426.324935], 17);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+  }).addTo(map);
 
-L.marker([-33.273946, -426.324935]).addTo(map)
-  .bindPopup('Este será el lugar de la conferencia')
-  .openPopup()
-  .bindTooltip('Un tooltip')
-  .openTooltip();
+  L.marker([-33.273946, -426.324935]).addTo(map)
+    .bindPopup('Este será el lugar de la conferencia')
+    .openPopup()
+    .bindTooltip('Un tooltip')
+    .openTooltip();
 
 }
 
@@ -23,8 +23,30 @@ L.marker([-33.273946, -426.324935]).addTo(map)
 $(document).ready(function(){
   // Lettering del logo GDLWebCamp
   $('.site-name').lettering();
+
+  // Código para dejar fijo el nav-bar
+  let windowHeight = $(window).height();
+  let navBarHeight= $('.nav-bar').innerHeight();
+
+  $(window).scroll(function () { 
+    const scroll = $(window).scrollTop();
+    
+    if(scroll > windowHeight){
+      $('.nav-bar').addClass('fixed');
+      $('body').css({'margin-top': navBarHeight+'px'});
+    }else{
+      $('.nav-bar').removeClass('fixed');
+      $('body').css({'margin-top': 0+'px'});
+    }
+
+  });
+
+  // Código para que el menú sea responsive
+  $('.btn-menu').on('click', function(){
+    $('.principal-nav').toggleClass('active');
+  })
   
-  // Código para los tabs del programa del evento en la páina index.html
+  // Código para los tabs del programa del evento en la página index.html
   $('div.hidden').hide();
 
   $('.program-event .course-info:first').show();
@@ -46,7 +68,7 @@ $(document).ready(function(){
   $('.event-summary li:nth-child(2) p').animateNumber({number: 15}, 3000);
   $('.event-summary li:nth-child(3) p').animateNumber({number: 3}, 3000);
   $('.event-summary li:nth-child(4) p').animateNumber({number: 9}, 3000);
-
+  
   // Cuenta regresiva
   $('.count-down').countdown('2021/12/10 09:00:00', function(event){
     $('#days').html(event.strftime('%D'));
